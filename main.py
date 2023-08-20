f442dc605f0992f84eb971464bb71737b629e285
AlekseiKostromin <cs777@yandex.ru>
2023-08-20 22:12:17 +0500

    Add gitignore

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "https://api.apilayer.com/exchangerates_data/latest?symbols={symbols}&base={base}"
+
+payload = {}
+headers= {
+  "apikey": "AIhlWtVlORMtBwO1JwhixuKk2wsNnWQK"
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+status_code = response.status_code
+result = response.text
